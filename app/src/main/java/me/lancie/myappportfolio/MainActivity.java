0bce3017c8da41f3d22abdcf9b30f41d2f8761b0
Lancie <lancie2@gmail.com>
2015-08-13 10:41:46 -0500

    Initial upload
    Base code, mostly working

---
+++ app/src/main/java/me/lancie/myappportfolio/MainActivity.java
@@ -0,0 +1,81 @@
+package me.lancie.myappportfolio;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        Button button2 = (Button) findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button3);
+        Button button4 = (Button) findViewById(R.id.button4);
+        Button button5 = (Button) findViewById(R.id.button5);
+        Button button6 = (Button) findViewById(R.id.button6);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void launchApp (View view) {
+        switch (view.getId()) {
+            case R.id.button1:
+                displayToast(getString(R.string.app1));
+                break;
+            case R.id.button2:
+                displayToast(getString(R.string.app2));
+                break;
+            case R.id.button3:
+                displayToast(getString(R.string.app3));
+                break;
+            case R.id.button4:
+                displayToast(getString(R.string.app4));
+                break;
+            case R.id.button5:
+                displayToast(getString(R.string.app5));
+                break;
+            case R.id.button6:
+                displayToast("CAPSTONE: MY OWN APP");
+                break;
+            default:
+                displayToast("This shouldn't be happening");
+        }
+    }
+
+    public void displayToast(CharSequence text) {
+        text = "This button will launch my " + text + " app!";
+        Toast toast = Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
