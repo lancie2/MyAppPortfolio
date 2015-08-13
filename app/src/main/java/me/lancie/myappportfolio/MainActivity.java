57c465c55e749a976161f022ca480daba86083ca
Lancie <lancie2@gmail.com>
2015-08-13 11:35:29 -0500

    Cleaned up onCreate in MainActivity

--- app/src/main/java/me/lancie/myappportfolio/MainActivity.java
+++ app/src/main/java/me/lancie/myappportfolio/MainActivity.java
@@ -15,16 +15,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button1 = (Button) findViewById(R.id.button1);
-        Button button2 = (Button) findViewById(R.id.button2);
-        Button button3 = (Button) findViewById(R.id.button3);
-        Button button4 = (Button) findViewById(R.id.button4);
-        Button button5 = (Button) findViewById(R.id.button5);
-        Button button6 = (Button) findViewById(R.id.button6);
-
-
-
     }
 
     @Override
